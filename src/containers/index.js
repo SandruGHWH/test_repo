6eff809f2ca6966a21a043abb79a8c28b91b0afb
GHWHrepo <pandelob8@gmail.com>
2023-02-14 12:31:34 +0100

    initial commit

---
+++ src/containers/index.js
@@ -0,0 +1,54 @@
+// HomePage
+export { default as Intro } from './HomePage/Intro';
+export { default as FarmSection } from './HomePage/FarmSection';
+export { default as FeaturesSection } from './HomePage/FeaturesSection';
+export { default as InfoSection } from './HomePage/InfoSection';
+
+//LiquidityPage
+export { default as LiquiditySwap } from './LiquidityPage/LiquiditySwap';
+
+// ExchangePage
+export { default as SwapSection } from './ExchangePage/SwapSection';
+export { default as LiquiditySection } from './ExchangePage/LiquiditySection';
+export { default as SliderBanner } from './ExchangePage/SliderBanner';
+
+// PoolsPage
+export { default as HeaderSection } from './PoolsPage/HeaderSection';
+export { default as OptionsHeader } from './PoolsPage/OptionsHeader';
+export { default as CardsSection } from './PoolsPage/CardsSection';
+export { default as LaunchpadContainer } from './PoolsPage/LaunchpadContainer';
+
+//FarmsPage
+export { default as FarmHeader } from './FarmsPage/FarmHeader';
+export { default as FarmOptions } from './FarmsPage/FarmOptions';
+export { default as FarmsContainer } from './FarmsPage/FarmsContainer';
+
+//TallyPay
+export { default as TallyWillsNew } from './TallyPay/TallyWillsNew';
+export { default as TallyWillsAdd } from './TallyPay/TallyWillsAdd';
+export { default as TallyWillsRemove } from './TallyPay/TallyWillsRemove';
+export { default as TallyWillsRecovery } from './TallyPay/TallyWillsRecovery';
+export { default as TallyWillsReport } from './TallyPay/TallyWillsReport';
+export { default as LivingTrustNew } from './TallyPay/LivingTrustNew';
+export { default as LivingTrustAdd } from './TallyPay/LivingTrustAdd';
+export { default as LivingTrustRemove } from './TallyPay/LivingTrustRemove';
+export { default as LivingTrustRecurring } from './TallyPay/LivingTrustRecurring';
+export { default as LivingTrustRecovery } from './TallyPay/LivingTrustRecovery';
+export { default as TempLockNew } from './TallyPay/TempLockNew';
+export { default as TempLockAdd } from './TallyPay/TempLockAdd';
+export { default as TempLockRemove } from './TallyPay/TempLockRemove';
+export { default as DefiAccount } from './TallyPay/DefiAccount';
+
+// TokenCheckerPage
+export { default as TokenCheckerHeading } from './TokenCheckerPage/TokenCheckerHeading';
+export { default as CoinsTable } from './TokenCheckerPage/CoinsTable';
+export { default as TokenCheckerInfo } from './TokenCheckerPage/TokenCheckerInfo';
+
+// NFTMarketPage
+export { default as Showcase } from './NFTMarketPage/Showcase';
+
+// AnalyticsPage
+export { default as AnalyticsHeading } from './AnalyticsPage/AnalyticsHeading';
+export { default as AreaChartContainer } from './AnalyticsPage/AreaChartContainer';
+export { default as BarChartContainer } from './AnalyticsPage/BarChartContainer';
+export { default as TopPoolsContainer } from './AnalyticsPage/TopPoolsContainer';
